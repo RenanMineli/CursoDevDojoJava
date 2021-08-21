d8ef0a63d8c704007fa4e5cb7880b46c4e4122b4
RenanLrMineli <renanmineli@hotmail.com>
2021-08-20 23:17:34 -0300

    Primeiro commit no GitHub

---
+++ src/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,24 @@
+/* Exercicio
+
+Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+Eu <nome>, morando no endereço <endereço>,
+confirmo que recebi o salário de <salário>, na data <data>
+ */
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Renan";
+        String endereço = "Rua Dardanelos 159, Pq. Novo Oratório Santo André-SP";
+        double salário = 8750.50;
+        String dataRecebimentoSalário = "13/08/2021";
+        String relatorio = "Eu " +nome+ ", morando no endereço " +endereço+ ", confirmo que recebi o salário de " +salário+ ", na data " +dataRecebimentoSalário;
+
+        //DUAS OPÇÕES PARA ESCREVER A FRASE
+        System.out.println(relatorio);
+        //OU
+        System.out.println("Eu " +nome+ ", morando no endereço " +endereço+
+                            ", confirmo que recebi o salário de " +salário+ ", na data " +dataRecebimentoSalário);
+    }
+
+}
---
+++ src/Aula04Operadores.java
@@ -0,0 +1,48 @@
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        double resultado = numero01 / (double) numero02;
+        System.out.println(resultado);
+
+        // % resto
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        //<> <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " + isDezIgualVinte);
+        System.out.println("isDezIgualDez " + isDezIgualDez);
+        System.out.println("isDezDiferenteDez " + isDezDiferenteDez);
+
+        //&& (AND) || (OR)
+        int idade = 29;
+        float salario = 3500f;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario <= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000f;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel " +isPlaystationCincoCompravel);
+
+
+        //= += -= *= /= %=
+        double bonus = 1000;
+        bonus = bonus + 1000;
+
+
+
+    }
+}
