6aeb02e7df8499717f85634dd69c380916d542d0
RenanLrMineli <renanmineli@hotmail.com>
2021-08-22 23:29:40 -0300

    video 35 devdojo

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -2,10 +2,14 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrays02 {
     public static void main(String[] args) {
-        int[] idades = new int[3];
+        String[] nomes = new String[4];
+        nomes [0] = "Goku";
+        nomes [1] = "Kurosaki";
+        nomes [2] = "Luffy";
+        nomes [3] = "Hinata";
 
-        System.out.println(idades[0]);
-        System.out.println(idades[1]);
-        System.out.println(idades[2]);
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
     }
 }
