94959697ce947f0d8b7f2e75eb8399a1c24dd972
Renan <renanmineli@hotmail.com>
2021-08-29 18:02:25 -0300

    Video 38 devdojo

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais02 {
+    public static void main(String[] args) {
+        int[][] arrayInt = new int[2][];
+
+        for (int[] arrayBase: arrayInt) {
+            for (int num: arrayBase) {
+                System.out.println(num);
+            }
+        }
+    }
+}
+
