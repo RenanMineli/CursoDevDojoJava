ef48289074b56ec4cd4b65721fec3a9b9353a809
RenanLrMineli <renanmineli@hotmail.com>
2021-09-05 22:59:25 -0300

    video 49

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImprimiEstudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        ImprimiEstudante imprimiEstudante = new ImprimiEstudante();
+
+        estudante01.nome = "Midoriya";
+        estudante01.idade = 15;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Sakura";
+        estudante02.idade = 16;
+        estudante02.sexo = 'F';
+
+        imprimiEstudante.imprimiEstudante(estudante01);
+        imprimiEstudante.imprimiEstudante(estudante02);
+    }
+}
